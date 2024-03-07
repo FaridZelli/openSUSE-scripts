1aefb33138cfc358b9d2e3751dddb0d1fca51100
Farid Zelli <82771409+FaridZelli@users.noreply.github.com>
2024-03-07 16:57:03 +0330

    Update suse_rusticl_radeon.sh

--- suse_rusticl_radeon.sh
+++ suse_rusticl_radeon.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# Script by Farid Zellipour
+# https://github.com/FaridZelli
+
 # Check the current user
 USER=$(whoami)
 if [ "$USER" == "root" ]; then
@@ -7,18 +10,29 @@ if [ "$USER" == "root" ]; then
   echo -e "\033[32mYou are logged in as root.\033[0m"
 else
   # Non-root user detected
-  echo -e "\033[31mATTENTION:\033[0m \033[33mYou do not seem to be logged in as root!\033[0m"
+  echo -e "
+--------------------------------------------------
+\033[33mWARNING: You do not seem to be logged in as root!\033[0m
+--------------------------------------------------"
 fi
-# Ask to procede with the rest of the script
-read -p "This script will install and configure Rusticl for Radeon on your system. Do you want to continue? (Y/N) " ANSWER
+
+# Ask whether to proceed
+echo -e "
+This script will reconfigure your \033[32mopenSUSE\033[0m \033[36mGNOME\033[0m environment.
+I am not responsible for any damage or data loss that may occur.
+
+\033[33mDo you wish to continue? (Y/N)\033[0m
+"
+# User input
+read -p "Your choice:" ANSWER
+# Read input
 case $ANSWER in
   [Yy]* ) 
-    # Procede with the rest of the script
-    echo "Starting..."
+    # Proceed with the rest of the script
     ;;
   * )
     # Stop the script for any other input
-    echo "Stopping..."
+    echo "Stopping the script..."
     exit 1
     ;;
 esac
@@ -26,7 +40,8 @@ esac
 zypper refresh
 zypper in Mesa-libRusticlOpenCL clinfo
 
-echo -e "--------------------
+echo -e "
+--------------------------------------------------
 \033[33mDone! Now exit root and run the following command as the primary user:\033[0m
 mkdir ~/.config/environment.d && echo "RUSTICL_ENABLE=radeonsi" > ~/.config/environment.d/rusticl.conf
---------------------"
+--------------------------------------------------"
